6eda7f0538c0f9cb30ca9a4fd981fb8dfa8afc0c
Natalie <nh365@sussex.ac.uk>
2022-03-08 14:14:55 +0000

    Programmed getColouredProperties method in Board class

--- Property Tycoon/Assets/Scripts/Board.cs
+++ Property Tycoon/Assets/Scripts/Board.cs
@@ -14,10 +14,20 @@ static public class Board
      * Returns: PropertyInfo array containing all the properties of colour 'pc'
      * Purpose: used to generate random dice roll and track number of doubles rolled
      */
-    static PropertyInfo[] getColouredProperties(propColour pc)
+    static BoardTile[] getColouredProperties(propColour pc)
     {
+        List<BoardTile> props = new List<BoardTile>();
+        foreach (BoardTile tile in boardSquares)
+        {
+            if (tile.propertyInfo.getPC = pc) 
+            {
+                props.Add(tile);
+            }
+        }
+        BoardTile[] propArray = props.ToArray();
+
         // Return all properties of colour pc
-        return null;
+        return propArray;
     }
 
     static public BoardTile findTile(int index)
