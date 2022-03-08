ae232602ad9eef5c4463bfdbd0cb9d38cd7ce605
Natalie <nh365@sussex.ac.uk>
2022-03-08 14:08:50 +0000

    Changed propertyInfo to public in BoardTile

--- Property Tycoon/Assets/Scripts/BoardTile.cs
+++ Property Tycoon/Assets/Scripts/BoardTile.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class BoardTile : MonoBehaviour
 {
-    PropertyInfo propertyInfo;
+    public PropertyInfo propertyInfo;
     public string tileName;
     public tileType type;
 
+    /*
+     * Function: getPropertyInfo
+     * Parameters: N/A
+     * Returns: propInfo class - the property info relating to this tile
+     * Purpose: to return details of the property on this tile
+     */
+    PropertyInfo getPropertyInfo()
+    {
+        return propertyInfo;
+        // Return property info for this tile
+    }
 }
