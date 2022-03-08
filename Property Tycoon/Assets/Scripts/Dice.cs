57168b47144b12d63cbd1916f07b596cc98eb563
Natalie <nh365@sussex.ac.uk>
2022-03-08 14:01:43 +0000

    Programmed Dice class

--- Property Tycoon/Assets/Scripts/Dice.cs
+++ Property Tycoon/Assets/Scripts/Dice.cs
@@ -15,6 +15,17 @@ public class Dice
      */
     public Dice()
     {
+        int dice1 = Random.Range(1, 7);
+        int dice2 = Random.Range(1, 7);
+        value = dice1 + dice2;
+        if (dice1 == dice2)
+        {
+            isDouble = true;
+        }
+        else
+        {
+            isDouble = false;
+        }
         // Randomises 2 1-6 values, adds them - puts value in value (if double, set double true else false) Random.Range(1,7);
     }
 }
