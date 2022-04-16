45310dfa0c0efa11658bb82c13474bd27c5f57eb
Josh <jas74@sussex.ac.uk>
2022-04-17 00:23:46 +0100

    UI buttons linked to GameUIManager functions (+skeleton code)

---
+++ Property Tycoon/Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    public void OnUpgradeClicked()
+    {
+
+    }
+
+    public void OnMortgageClicked()
+    {
+
+    }
+
+    public void OnUnMortgageClicked()
+    {
+
+    }
+
+    public void OnSellClicked()
+    {
+
+    }
+
+    public void OnRTDClicked()
+    {
+
+    }
+
+    public void OnEndTurnClicked()
+    {
+
+    }
+
+    public void OnPurchaseClicked()
+    {
+
+    }
+
+    public void OnAuctionClicked()
+    {
+
+    }
+
+    public void OnUpgradeDropdownClicked()
+    {
+
+    }
+
+    public void OnSellDropdownClicked()
+    {
+
+    }
+
+    public void OnMortgageDropdownClicked()
+    {
+
+    }
+
+    public void OnUnMortgageDropdownClicked()
+    {
+
+    }
+
+    public void OnBackClicked()
+    {
+
+    }
+}
