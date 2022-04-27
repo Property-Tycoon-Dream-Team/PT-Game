44c06590544cd3f1aaa12006b773ce3bbc77c1d0
Joe Holman <jh760@sussex.ac.uk>
2022-04-27 15:02:15 +0100

    selectedProperty to BoardTile

--- Property Tycoon/Assets/Scripts/GameManager.cs
+++ Property Tycoon/Assets/Scripts/GameManager.cs
@@ -32,7 +32,7 @@ public class GameManager : MonoBehaviour
     public GameObject[] playerInfoEntry;
     public BoardTile[] tiles;
     public GameObject tileParent;
-    public PropertyInfo selectedProperty;
+    public BoardTile selectedProperty;
 
     private Player[] players;
     private int activePlayerID;
