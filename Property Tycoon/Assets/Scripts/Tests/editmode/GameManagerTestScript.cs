0d1902e66d8069c21f27f4a7b44713940bd577b6
Louie <lm683@sussex.ac.uk>
2022-04-28 19:43:34 +0100

    Tests done for playersLeft and addToFreeParking ->>
    other tests created fail due to a Null Reference Exception (ffs)

--- Property Tycoon/Assets/Scripts/Tests/editmode/GameManagerTestScript.cs
+++ Property Tycoon/Assets/Scripts/Tests/editmode/GameManagerTestScript.cs
@@ -5,110 +5,100 @@ using UnityEngine;
 using UnityEngine.TestTools;
 
 public class GameManagerTestScript
-{ 
-   
-    [Test]
-    public void TestOnPlayerInfoChange()
-    {
-        GameManager gm = new GameManager(); 
-        gm.onPlayerInfoChange();
-    }
+{
+    private GameManager gm;
 
+    [SetUp]
+    public void SetUp()
+    {
+        gm = new GameManager();
+    }
+   
     [Test]
     public void TestRollDice()
     {
-        var gm = new GameManager();
         gm.rollDice(true);
     }
 
     [Test]
-    public void TestGetTileObject()
+    public void TestAddToFreeParking()
     {
-        GameManager gm = new GameManager();
-        gm.getTileObject(5);
-    }
+        int amount = 50;
+        gm.addToFreeParking(amount);
+        Assert.That(gm.freeParkingValue >= amount);
 
-    [Test]
-    public void TestOnGamemodeDropdownChange()
-    {
-        GameManager gm = new GameManager();
-        gm.onGamemodeDropdownChange();
-    }
-
-    [Test]
-    public void TestOnSliderChange()
-    {
-        GameManager gm = new GameManager();
-        gm.onSliderChange();
-    }
-
-    [Test]
-    public void TestStartGame()
-    {
-        GameManager gm = new GameManager();
-        gm.startGame();
     }
 
     [Test]
     public void TestPlayersLeft()
     {
-        GameManager gm = new GameManager();
-        gm.playersLeft();
-    }
-
-    [Test]
-    public void TestPlayerSetupe()
-    {
-        GameManager gm = new GameManager();
-        gm.onSliderChange();
+        Player player = new Player();
+        Player player2 = new Player();
+        gm.players = new Player[] { player, player2 };
+        Assert.That(gm.playersLeft() == 2);       
     }
 
     [Test]
     public void Testpurchase()
     {
-        GameManager gm = new GameManager();
         gm.purchase();
     }
 
     [Test]
     public void TestUpgrade()
     {
-        GameManager gm = new GameManager();
         gm.upgrade();
     }
 
     [Test]
     public void TestAuction()
     {
-        GameManager gm = new GameManager();
         gm.auction();
     }
 
     [Test]
     public void Testmortgage()
     {
-        GameManager gm = new GameManager();
-        gm.mortgage();
+        Player p = new Player();
+        BoardTile bt = new BoardTile();
+        gm.tiles = new BoardTile[] {bt};
+        gm.messager = new SystemMsgs();
+        gm.selectedProperty = gm.tiles[0];
+        bt.cost = 500;
+        p.cash = 0;
+        gm.activePlayer = p;
+        Assert.IsTrue(gm.mortgage());
+
     }
 
     [Test]
     public void TestUnMortgage()
     {
-        GameManager gm = new GameManager();
         gm.unMortgage();
     }
 
     [Test]
     public void TestSell()
     {
-        GameManager gm = new GameManager();
-        gm.sell();
+        BoardTile bt = new BoardTile();
+        gm.selectedProperty = bt;
+        Player p = new Player();
+        p.ownedProperties.Add(bt);
+        gm.activePlayer = p;
+        Assert.IsTrue(gm.sell());
     }
 
     [Test]
     public void TestEndTurn()
     {
-        GameManager gm = new GameManager();
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+
+        gm.players = new Player[] { p1, p2, p3 };
+        gm.activePlayer = p1;
         gm.endTurn();
+        Assert.That(gm.activePlayer == p2);
+        
     }
 }
