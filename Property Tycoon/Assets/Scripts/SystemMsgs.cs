ef6c2df6ffc2687012f0c4c927a7b1f9c2cfa28e
Josh <jas74@sussex.ac.uk>
2022-04-28 18:57:53 +0100

    Stacking system messages now

--- Property Tycoon/Assets/Scripts/SystemMsgs.cs
+++ Property Tycoon/Assets/Scripts/SystemMsgs.cs
@@ -3,29 +3,40 @@ using UnityEngine.UI;
 
 public class SystemMsgs : MonoBehaviour
 {
-    public float wait = 3;
+    public float wait = 3f;
+    float bigTimer;
+    float timer = 3f;
     public Text msg;
-    float timer;
     bool msgTriggerd = false;
 
     void Update()
     {
         if (msgTriggerd)
         {
+            bigTimer -= Time.deltaTime;
             timer -= Time.deltaTime;
-            if (timer <= 0)
+
+            if (timer <= 0f)
+            {
+                msg.text = msg.text.Substring(msg.text.IndexOf('\n') + 1);
+                timer = 3f;
+            }
+            if (bigTimer <= 0f)
+            {
+                msg.text = "";
+            }
+
+            if (timer <= 0f && bigTimer <= 0f)
             {
                 msgTriggerd = false;
-                gameObject.SetActive(false);
             }
         }
     }
 
     public void NewMessage(string newMsg)
     {
-        msg.text = newMsg;
+        msg.text += newMsg + "\n";
         msgTriggerd = true;
-        timer = wait;
-        gameObject.SetActive(true);
+        bigTimer = wait;
     }
 }
